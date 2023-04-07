ad5e50934d68cbd1d6bd68eaf01c72958f02f012
SeopBong <gkstjq309@gmail.com>
2023-04-07 19:20:42 +0900

    imageAdd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
+import Main from './Main';
+
 
 function App() {
+
+  let [mainBtnColor, setMainBtnColor] = useState('#FFFFFF');
+  let [textColor, setTextColor] = useState('#F37116');
+
+
+  const handleMouseEnter = ()=>{
+    setMainBtnColor('#F37116');
+    setTextColor('#FFFFFF');
+    
+  };
+  
+  const handleMouseLeave = ()=>{
+    setMainBtnColor('#FFFFFF');
+    setTextColor('#F37116');
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <img className='logo1' src='./crossfitable.jpg'></img>
+        <div>
+          <Link to={"/Main"}>
+          <button className='munjung-btn' style={{ backgroundColor:mainBtnColor, color: textColor}} 
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave} >문정점</button>
+          </Link>
+        </div>
+      </div>
+      <>
+        <Routes>
+        <Route path='/' element={<div>시작페이지임</div>}></Route>
+      </Routes>
+      </>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
+
+function Main(){
+
+    return(
+        <div>여기가 시작</div>
+
+    );
+}
